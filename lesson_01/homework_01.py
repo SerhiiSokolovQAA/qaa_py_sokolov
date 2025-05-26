a4584919e89e7d41761cfd2f5b72c2ba2bcf616b
Serhii <s.sokolov.qa@gmail.com>
2025-05-26 09:47:54 +0300

    some text

---
+++ lesson_01/homework_01.py
@@ -0,0 +1,58 @@
+# task 01
+print("Hello", end = " ")
+print("world!")
+
+# task 02
+hello = "Hello"
+world = "world"
+if True:
+    print("{hello} {world}!")
+
+# task 03
+for letter in "Hello world!":
+    print(letter)
+
+# task 04
+apples = 2
+banana = apples * 4
+
+# task 05 // от себя ->
+# переменная будет работать и так и так,
+# но т.к. 3 из 4 переменных написаны латинскими буквами,
+# то решил делать все в одном виде
+storona_1 = 1
+storona_2 = 2
+storona_3 = 3
+storona_4 = 4
+
+# task 06
+perimetery = (storona_1 + storona_2 + storona_3 + storona_4)
+print(perimetery)
+
+# task 07
+apple_tree = 4
+pear_tree = apple_tree + 5
+plum_tree = apple_tree - 2
+total_trees = apple_tree + pear_tree + plum_tree
+print(total_trees)
+
+# task 08
+temp_before_supper = 5
+temp_after_supper = temp_before_supper - 10
+evening_temp = temp_after_supper + 4
+print(evening_temp)
+
+# task 09
+boys = 24
+girls = boys // 2
+sick_boys = 1
+absent_girls = 2
+total_present = boys + girls - sick_boys - absent_girls
+print(total_present)
+
+# task 10
+first_book = 8.00
+second_book = first_book + 2.00
+third_book = (first_book + second_book) / 2
+total_cost = first_book + second_book + third_book
+print(total_cost)
